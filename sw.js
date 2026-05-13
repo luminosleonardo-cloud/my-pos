12f11505f7190883741f211b33159ad6fac7a457
luminosleonardo-cloud <luminosleonardo@gmail.com>
2026-05-13 17:11:32 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    update cache in background → next load gets fresh files.
    ============================================================ */
 
-const CACHE_NAME = 'pos-v2.11';
+const CACHE_NAME = 'pos-v2.12';
 
 const LOCAL_ASSETS = [
   './',
